4e08a69a681f44dbdde86877f77338a121d39567
ALEXANDER <belligham.gordillo05@gmail.com>
2026-04-21 13:52:57 -0500

    v1.errores.py

--- question1.py
+++ question1.py
@@ -2,5 +2,26 @@
 
 Implementa una CLI que calcule carga por punto de soporte.
 """
+import sys
 
-# TODO: Implementar según README.md
+try:
+    if len(sys.argv) != 3:
+        raise ValueError
+
+
+    total_load = float(sys.argv[1]) 
+    num_supports = int(sys.argv[2])
+    if num_supports == 0:
+        raise ValueError   
+     
+    load_per_support = total_load / num_supports  
+
+    print(f"Carga por punto de soporte: {load_per_support:.2f}")
+
+
+except ValueError:
+    print("Error: input invalido! introduzca solo valores numericos")
+          
+except ZeroDivisionError:
+    print("Error: ¡ No se puede dividir por cero! El numero de puntos de soporte debe ser mayor que cero.")
+
--- todo_manager.py
+++ todo_manager.py
@@ -3,11 +3,18 @@
 
 def read_todo_file(file_path):
     """Reads tasks from a file. Returns a list of tasks."""
-    # TODO: Implementar manejo de FileNotFoundError según README.md
+    try:
+        with open (file_path, 'r') as file:
+            return file.read().splitlines()
+    except FileNotFoundError:
+        print(f"archivo {file_path} no encontrado. Se creará un nuevo archivo al guardar tareas.")
+        return []    
     raise NotImplementedError
 
 
 def write_todo_file(file_path, tasks):
     """Writes tasks to a file, one per line."""
-    # TODO: Implementar escritura de tareas según README.md
+    with open(file_path, 'w') as file:
+        for task in tasks:
+            file.write(task + '\n')
     raise NotImplementedError
